dfb89b0f34c593e6fb95b880edd2538696535af6
yeah-caleb <caleb.meeker@mvschool.com>
2026-01-29 12:35:54 -0500

    wave tracking

--- components/screens/game_world.gd
+++ components/screens/game_world.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
 @onready var levelWaves = $waveHandler
+@onready var label = $Label
 
 @onready var kickBall = preload("res://components/classes/enemies/kickball.tscn")
 @onready var seagull = preload("res://components/classes/enemies/seagull.tscn")
-
 
 @onready var spawn1 = $enemySpawner1
 @onready var spawn2 = $enemySpawner2
@@ -14,6 +14,10 @@ var activeWave = false
 
 var enemyPool
 var enemySelect
+var waveTime
+
+var currentWave = 0
+var curWaveDifficulty = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,6 +26,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	spawnCycle()
+	updateLabel()
 
 func spawnCycle():
 	if !spawning && activeWave:
@@ -37,6 +42,18 @@ func spawnCycle():
 				newEnemy.global_position = spawn1.global_position
 			else:
 				newEnemy.global_position = spawn2.global_position
-		
-		await get_tree().create_timer(2.0).timeout
+		if currentWave*0.1 < 2.0:
+			waveTime = currentWave*0.1
+		else:
+			waveTime = 1.9
+		await get_tree().create_timer(2.0-waveTime).timeout
 		spawning = false
+		
+		if newEnemy == null:
+			currentWave += 1
+			curWaveDifficulty += 25
+			levelWaves.Wave(enemyPool, curWaveDifficulty)
+			print(currentWave)
+
+func updateLabel():
+	label.text = "Current Wave: " + str(currentWave)
