9a841cf580b1dfe69118adaaed578ca4119e51f6
kirinahamila <kirinahamila@pm.me>
2026-01-30 11:47:35 -0500

    add props to menu

--- system/ui/selection/prop_selection_menu.gd
+++ system/ui/selection/prop_selection_menu.gd
@@ -10,6 +10,10 @@ signal propPlacementTime
 @onready var platForm = preload("res://components/prop/platform.tscn")
 @onready var fan = preload("res://components/prop/fan.tscn")
 @onready var flameThrower = preload("res://components/prop/flame_thrower.tscn")
+@onready var plank = preload("uid://cm45ttyql1yvc")
+@onready var sack = preload("uid://d2g4eihw6i2ld")
+@onready var slab = preload("uid://b1lyoa25pp8nc")
+
 
 @onready var descBox = $descBox
 @onready var grid = $GridContainer
